b0e6a712cef7e204b4c1dff164c3c76e7d7637b8
Matti Kantola <matti.kantola@helsinki.fi>
2022-09-21 10:09:06 +0300

    Kolmostehtävän eka kohta melkein valmis

--- Tehtava3/main.py
+++ Tehtava3/main.py
@@ -2,7 +2,7 @@ import random
 import string
 import sqlite3
 import os
-from timeit import timeit
+from time import time
 
 data = sqlite3.connect("testielokuvat.db")
 
@@ -16,4 +16,35 @@ def ei_indeksia():
 
     data = sqlite3.connect("testielokuvat.db")
 
-    lisays_alku = timeit()
+    data.execute("CREATE TABLE Elokuvat (id INTEGER PRIMARY KEY, nimi TEXT, vuosi INTEGER)")
+
+    lisays_alku = time()
+
+    data.execute("BEGIN")
+
+    for iii in range(10**6):
+        
+        nimi, vuosi = elokuva(8,1900,2000)
+        data.execute(f"INSERT INTO Elokuvat (nimi, vuosi) VALUES ('{nimi}', {vuosi})")
+
+    data.execute("COMMIT")
+
+    lisays_loppu = time()
+
+    kysely_alku = time()
+
+    cur = data.cursor()
+
+    for iii in range(1000):
+
+        julkaisuvuosi = random.randint(1900,2000)
+
+        cur.execute(f"SELECT COUNT(*) FROM Elokuvat WHERE vuosi = {julkaisuvuosi}")
+
+    kysely_loppu = time()
+
+    return lisays_loppu - lisays_alku, kysely_loppu - kysely_alku
+    
+if __name__ == "__main__":
+
+    print(ei_indeksia())
